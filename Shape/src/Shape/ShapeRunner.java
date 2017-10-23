d6fc023511e4df28e5fe4f431a5cae2550707cda
Derek <dyu5074@bths.edu>
2017-10-22 20:24:12 -0400

    Final Shape

--- Shape/src/Shape/ShapeRunner.java
+++ Shape/src/Shape/ShapeRunner.java
@@ -20,15 +20,14 @@ public class ShapeRunner {
 			System.out.println(shape);
 		}
 		System.out.println("\nDid the three shapes above add up to the total printed here?\n");
-		System.out.println("Total Area: " + ShapeUtilities.sumArea(shapeArr)); // 168.26
-		System.out.println("Total Perimeter: " + ShapeUtilities.sumPerimeter(shapeArr)); // 86.84
+		System.out.println("Total Area: " + ShapeUtilities.sumArea(shapeArr)); 
+		System.out.println("Total Perimeter: " + ShapeUtilities.sumPerimeter(shapeArr)); 
 		System.out.println("\nDid the three shapes above add up to the total printed here?\n");
 
 		System.out.println("\n\n Now testing the random array.\n");
 
 		shapeArr = new Shape[10];
 
-		// Why are we using a for loop instead of a for-each loop here?
 		for (int i = 0; i < shapeArr.length; i++) {
 			shapeArr[i] = ShapeUtilities.randomShape();
 		}
