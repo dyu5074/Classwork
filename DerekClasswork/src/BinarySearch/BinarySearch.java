ae14a2a310ce1f75420d009d1a2fe11f56d81b18
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-11-15 13:16:58 -0500

    d

--- DerekClasswork/src/BinarySearch/BinarySearch.java
+++ DerekClasswork/src/BinarySearch/BinarySearch.java
@@ -3,19 +3,32 @@ package BinarySearch;
 public class BinarySearch {
 	public static void main (String[] args)
 	{
-		int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
+		//int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		//int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
 		
-		System.out.println(binarySearch(test1, 3, 0, test1.length)); //2
-		System.out.println(binarySearch(test2, 23, 0, test2.length)); //7
-		System.out.println(binarySearch(test1, 1, 0, test1.length)); //0
-		System.out.println(binarySearch(test2, 25, 0, test2.length)); //-1, may cause an error, 
+		//System.out.println(binarySearch(test1, 3, 0, test1.length)); //2
+		//System.out.println(binarySearch(test2, 23, 0, test2.length)); //7
+		//System.out.println(binarySearch(test1, 1, 0, test1.length)); //0
+		//System.out.println(binarySearch(test2, 25, 0, test2.length)); //-1, may cause an error, 
 																	  // fix error message
+		
+		int [] test1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int [] test2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+		for (int i = -1; i < test1.length+1;i++)
+		{
+			System.out.print(binarySearch(test1, i, 0, test1.length) + " ");
+		}
+		System.out.println();
+		for (int i = -1; i < test2.length+1; i++)
+		{
+			System.out.print(binarySearch(test2, i, 0, test2.length) + " ");
+		}
+		System.out.println();
 	}
 	
 	public static int binarySearch(int[] nums, int query, int first, int last)
 	{
-		if (last >= first) //if still have some array left to search
+		if (last > first) //if still have some array left to search
 		{
 			int index = (first + last)/2;
 			int guess = nums[index];
@@ -27,9 +40,8 @@ public class BinarySearch {
 			
 			if (guess > query)
 			{
-				return binarySearch(nums, query, first, index - 1);
+				return binarySearch(nums, query, first, index);
 			}
-		// return binarySearch(nums, query, index, last);
 			return binarySearch(nums, query, index + 1, last);
 		}
 		
